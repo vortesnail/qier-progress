e19a5710903af5c99c030208042824cbd746b9bf
vortesnail <1091331061@qq.com>
2020-02-11 03:32:43 +0800

    docs: add demo page

---
+++ gh-page/precache-manifest.fd82d17828c38e0e943e822b4c79cf7d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce5b3677fc3f395b3a575665add2a50b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "59677a9e521105542554",
+    "url": "./static/css/main.d8854b64.chunk.css"
+  },
+  {
+    "revision": "0093b70f14f53d0ac268",
+    "url": "./static/js/2.2f07a25b.chunk.js"
+  },
+  {
+    "revision": "afa90853da04a54e8871725eeacc9540",
+    "url": "./static/js/2.2f07a25b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59677a9e521105542554",
+    "url": "./static/js/main.f1e0822c.chunk.js"
+  },
+  {
+    "revision": "052749a1beaaf8ce7921",
+    "url": "./static/js/runtime-main.37805f13.js"
+  },
+  {
+    "revision": "af6eddc6ebb0281e41872539e1df96d9",
+    "url": "./static/media/github.af6eddc6.svg"
+  },
+  {
+    "revision": "894a268d9ff23e3edfb86cded72b6c8e",
+    "url": "./static/media/logo.894a268d.png"
+  }
+]);
